a2432d0ae87583083f541d8df2d5e39038fe87c4
Batuhan Çam <batuc06@gmail.com>
2019-05-07 10:23:41 +0300

    adding custom login form and logout

--- src/main/java/com/springsecurityapp/config/AppSecurityConfig.java
+++ src/main/java/com/springsecurityapp/config/AppSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.springsecurityapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +13,8 @@ import org.springframework.security.core.userdetails.User;
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //add out users for in memory authentication
@@ -19,5 +23,19 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("batuhan").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("halil").password("test123").roles("MANAGER"))
                 .withUser(users.username("abdullah").password("test123").roles("ADMIN"));
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and()
+                .formLogin().loginPage("/showLoginPage")
+                .loginProcessingUrl("/authenticateUser")
+                .permitAll()
+                .and()
+                .logout();
+
+
     }
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -1,8 +1,13 @@
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
     <title>Welcome Home Page</title>
 </head>
 <body>
 <h2>Home Page</h2>
+
+<form:form action="${pageContext.request.contextPath}/logout" method="post">
+    <input type="submit" value="Logout">
+</form:form>
 </body>
 </html>
